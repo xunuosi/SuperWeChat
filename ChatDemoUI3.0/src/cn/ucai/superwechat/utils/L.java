ecc6db97d73a4a168bbac0c786a1d2163e00ee07
xunuosi <xunuosi@163.com>
2016-11-01 15:05:38 +0800

    添加了Gson、okHttp和相关工具类

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/L.java
@@ -0,0 +1,57 @@
+package cn.ucai.superwechat.utils;
+
+import android.util.Log;
+
+public class L {
+    // 控制台输出工具类的控制开关
+    public static boolean isDebug = true;
+    private static final String TAG = "fulicenter";
+
+    public static void i(String msg){
+        if(isDebug) {
+            Log.i(TAG, msg);
+        }
+    }
+
+    public static void d(String msg){
+        if(isDebug) {
+            Log.d(TAG, msg);
+        }
+    }
+
+    public static void e(String msg){
+        if(isDebug) {
+            Log.e(TAG, msg);
+        }
+    }
+
+    public static void v(String msg){
+        if(isDebug) {
+            Log.v(TAG, msg);
+        }
+    }
+
+    public static void i(String tag, String msg){
+        if(isDebug) {
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg){
+        if(isDebug) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg){
+        if(isDebug) {
+            Log.e(tag, msg);
+        }
+    }
+
+    public static void v(String tag, String msg){
+        if(isDebug) {
+            Log.v(tag, msg);
+        }
+    }
+}
