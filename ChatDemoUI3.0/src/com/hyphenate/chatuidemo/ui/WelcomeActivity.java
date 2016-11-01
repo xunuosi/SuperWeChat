38c008ab61bb49e920f3ddcac1c41cdcd8f9e185
xunuosi <xunuosi@163.com>
2016-11-01 09:42:24 +0800

    添加了ButterKnife的依赖

--- ChatDemoUI3.0/src/com/hyphenate/chatuidemo/ui/WelcomeActivity.java
+++ ChatDemoUI3.0/src/com/hyphenate/chatuidemo/ui/WelcomeActivity.java
@@ -1,8 +1,13 @@
 package com.hyphenate.chatuidemo.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hyphenate.chatuidemo.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WelcomeActivity extends BaseActivity {
 
@@ -10,5 +15,18 @@ public class WelcomeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login_wel, R.id.btn_register_wel})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login_wel:
+
+                break;
+            case R.id.btn_register_wel:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
     }
 }
