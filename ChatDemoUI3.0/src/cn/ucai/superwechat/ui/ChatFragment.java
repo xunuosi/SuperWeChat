36976a8a05fa98b8403312197d6c1adb1d972c48
xunuosi <xunuosi@163.com>
2016-11-09 15:26:54 +0800

    增加了对话界面点击头像跳转到好友信息界面的功能

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ChatFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ChatFragment.java
@@ -32,6 +32,7 @@ import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.domain.EmojiconExampleGroupData;
 import cn.ucai.superwechat.domain.RobotUser;
+import cn.ucai.superwechat.utils.MFGT;
 import cn.ucai.superwechat.widget.ChatRowVoiceCall;
 import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.ui.EaseChatFragment;
@@ -251,12 +252,19 @@ public class ChatFragment extends EaseChatFragment implements EaseChatFragmentHe
         }
     }
 
+    /**
+     * 聊天对话框中监听点击头像事件
+     * @param username
+     */
     @Override
     public void onAvatarClick(String username) {
         //handling when user click avatar
-        Intent intent = new Intent(getActivity(), UserProfileActivity.class);
+
+        /*Intent intent = new Intent(getActivity(), UserProfileActivity.class);
         intent.putExtra("username", username);
-        startActivity(intent);
+        startActivity(intent);*/
+        // 点击好友头像跳转到好友信息界面
+        MFGT.gotoFriendProfile(getActivity(), SuperWeChatHelper.getInstance().getAppContactList().get(username));
     }
     
     @Override
