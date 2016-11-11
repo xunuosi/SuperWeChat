bbe1d2af740c33afb33dd05740b66a7b2549dce8
xunuosi <xunuosi@163.com>
2016-11-11 11:33:27 +0800

    对话列表显示群头像功能

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -13,6 +13,7 @@ import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.Group;
 import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
@@ -138,11 +139,11 @@ public class EaseUserUtils {
         if(Hxid != null){
             try {
                 // avatarResId算是键值，头像以键值对的形式缓存在内存和磁盘当中
-                int avatarResId = Integer.parseInt(Hxid);
+                int avatarResId = Integer.parseInt(Group.getGroupIcon(Hxid));
                 Glide.with(context).load(avatarResId).into(imageView);
             } catch (Exception e) {
                 //use default avatar
-                Glide.with(context).load(Hxid).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_avatar).into(imageView);
+                Glide.with(context).load(Group.getGroupIcon(Hxid)).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_avatar).into(imageView);
             }
         }else{
             Glide.with(context).load(R.drawable.ease_group_icon).into(imageView);
